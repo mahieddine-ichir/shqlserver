381e526a683c20555eab2468522eced8e8576f98
michir <mahieddine.ichir@docapost.fr>
2018-01-03 11:50:17 +0100

    Stream operated on issue fix

--- src/main/java/com/michir/SQLCommandAliasParser.java
+++ src/main/java/com/michir/SQLCommandAliasParser.java
@@ -1,19 +1,15 @@
 package com.michir;
 
-import java.util.stream.Stream;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class SQLCommandAliasParser {
 
-	Stream<Alias> aliases = Alias.all();
-	
 	boolean contains(final String alias) {
-		return aliases.anyMatch(a -> a.matches(alias));
+		return Alias.all().anyMatch(a -> a.matches(alias));
 	}
 	
 	String parse(String input) {
-		return aliases.filter(a -> a.matches(input)).findFirst().map(a -> a.parse(input)).get();
+		return Alias.all().filter(a -> a.matches(input)).findFirst().map(a -> a.parse(input)).get();
 	}
 }
